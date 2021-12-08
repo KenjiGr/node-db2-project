e95b4de9515ee9c7397a2f3fc8cf6e9c58cfa883
Kenji Greene <kenjigrr@gmail.com>
2021-12-08 01:24:31 -0600

    set up migrations and model

--- api/cars/cars-model.js
+++ api/cars/cars-model.js
@@ -1,11 +1,22 @@
-const getAll = () => {
-  // DO YOUR MAGIC
+const db = require('../../data/db-config');
+
+const getAll = async () => {
+  const rows = await db('cars')
+  return rows
 }
 
-const getById = () => {
-  // DO YOUR MAGIC
+const getById = async (id) => {
+  const row = await db('cars')
+    .where('id', id)
+    .first()
+  return row
 }
 
-const create = () => {
-  // DO YOUR MAGIC
+const create = async (car) => {
+  const [carId] = await db('cars')
+    .insert(car)
+  const newCar = await getById(carId);
+  return newCar
 }
+
+module.exports = {getAll, getById, create}
--- api/server.js
+++ api/server.js
@@ -1,7 +1,8 @@
-const express = require("express")
-
-const server = express()
-
+const express = require("express");
+const carRouter = require('./cars/cars-router');
+const server = express();
+server.use(express.json());
+server.use('/api/cars', carRouter)
 // DO YOUR MAGIC
 
 module.exports = server
