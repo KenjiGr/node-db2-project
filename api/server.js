e3689113db5e0c20a2bfb14dc371187122d4e47c
Kenji Greene <kenjigrr@gmail.com>
2021-12-08 05:32:08 -0600

    mvp

--- api/server.js
+++ api/server.js
@@ -2,7 +2,6 @@ const express = require("express");
 const carRouter = require('./cars/cars-router');
 const server = express();
 server.use(express.json());
-server.use('/api/cars', carRouter)
-// DO YOUR MAGIC
+server.use('/api/cars', carRouter);
 
 module.exports = server
